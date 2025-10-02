a75241c19569c0dbd1fba0db1b35579f15440555
Devansh Gupta <Devanshgupta1942@gmail.com>
2025-10-02 10:57:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9287354951-58180",
+  "appId": "1:996399668469:web:a92a4289a9e54f7aa0f9bc",
+  "apiKey": "AIzaSyDvSYNkS6XXJywas9OSGRL7duZ-oCBw_cg",
+  "authDomain": "studio-9287354951-58180.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "996399668469"
+};
